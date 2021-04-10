b4e8e1a1d54913a803bcc887239a084b5700650a
KhanhLieu <khanhlieu1710@gmail.com>
2021-04-10 11:16:47 +0700

    Added all api method

---
+++ src/api/index.js
@@ -0,0 +1,30 @@
+// Import thư viện axios dùng để gọi api
+import axios from "axios";
+
+/**
+ * Call api truyền thống bằng axios:
+ * axios({ 
+ *    url: "http://localhost:5000/api/products/getAllProdút",
+ *    method: "Phương thức GET/POST/...",
+ *    data: "Nếu có"
+ * })
+ *    .then((result)=>{"Call Api thành công !"});
+ *    .catch((err)=>{"Call Api không thành công !"});
+ */
+
+const callApi = axios.create({
+  baseURL: "http://localhost:5000/api"
+});
+
+export default callApi;
+
+/**
+ * Tuy nhiên dự án call Api rất nhiều lần ở nhiều file khác nhau nên axios cung cấp
+ * phương thức create trả về một đối tượng có các chức năng GET/POST ..
+ * Cách gọi Api mới:
+ * Ex: callApi.get("/products/getAllProducts").then().catch(); // then, catch như trên
+ * Do mình đã tạo baseURL rồi nên bây giờ chỉ cần gõ phần còn lại của link Api thôi
+ * Cách này lợi ở :
+ *    + Code ngắn gọn hơn
+ *    + Sau này khi deploy backend ra thì chỉ cần vô đây sửa lại baseURL thô, không cần phải sửa nhiều nơi như cách kia
+ */
