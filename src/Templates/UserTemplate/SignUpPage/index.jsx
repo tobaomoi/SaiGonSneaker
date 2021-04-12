08a769642aa4e9239dfc3e325b9c77834e5b5bcd
MinhNhat <imminhat@gmail.com>
2021-04-12 13:10:50 +0700

    signup update

--- src/Templates/UserTemplate/SignUpPage/index.jsx
+++ src/Templates/UserTemplate/SignUpPage/index.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import './index.scss';
 function SignUpPage(props) {
 	return (
-		<div className="split-screen">
-			<form className="right" >
-				<h2 className="text-center">Sign Up</h2>
+		<div className="sign_Up_Page">
+			<form className="sign_Up_Form" >
+				<h2 className="text-center">Đăng kí</h2>
 				<div className="input-container name">
-					<label htmlFor="name">Full Name</label>
-					<input id="name" name="name" type="text" />
+					<label htmlFor="name">Tên hiển thị:</label>
+					<input id="name" name="name" placeholder="Vui lòng nhập tên hiển thị ..." type="text" />
 				</div>
 				<div className="input-container email">
-					<label htmlFor="ename">Email</label>
-					<input id="email" name="email" type="email" />
+					<label htmlFor="ename">Email:</label>
+					<input id="email" name="email" type="email" placeholder="Vui lòng nhập email ..." />
 				</div>
 				<div className="input-container password">
-					<label htmlFor="password">Password</label>
-					<input id="password" name="password" placeholder="Must be at least 6 characters" type="password" />
+					<label htmlFor="password">Mật khẩu</label>
+					<input id="password" name="password" placeholder="Vui lòng nhập mật khẩu ..." type="password" />
 					<i className="far fa-eye-slash"></i>
 				</div>
 				<div className="input-container confirmPassword">
-					<label htmlFor="password">Confirm password</label>
-					<input id="confirmPassword" name="confirmPassword" placeholder="Must be at least 6 characters" type="password" />
+					<label htmlFor="password">Mật khẩu xác nhận:</label>
+					<input id="confirmPassword" name="confirmPassword" placeholder="Vui lòng nhập mật khẩu xác nhận ..." type="password" />
 					<i className="far fa-eye-slash"></i>
 				</div>
 				<div className="input-container cta">
@@ -34,11 +34,11 @@ function SignUpPage(props) {
 				<button className="singup-btn" type="submit">
 					Sign up
                 </button>
-				<section className="copy">
+				<div>
 					<div className="login-container">
 						<p>Alredy have an account? <a href="#"><strong>Log In </strong></a> </p>
 					</div>
-				</section>
+				</div>
 			</form>
 		</div>
 	);
